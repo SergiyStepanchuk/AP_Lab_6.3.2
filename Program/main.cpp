6f383ecaf01826eab4b19bc5ff7bea8bbb8f1b34
SergiyStepanchuk <prochliste@gmail.com>
2020-11-18 21:23:24 +0200

    dorob

--- Program/main.cpp
+++ Program/main.cpp
@@ -3,12 +3,6 @@
 #include <iomanip>
 using namespace std;
 
-int myrand(const int& min, const int& max) {
-	return min > 0 ?
-		rand() % (max - min + 1) + min :
-		rand() % (abs(min) + max + 1) + min;
-}
-
 void draw_line(const int& size, const char& c = '-') {
 	cout << c;
 	if (size > 1) {
@@ -16,6 +10,14 @@ void draw_line(const int& size, const char& c = '-') {
 		return;
 	}
 	cout << endl;
+}
+
+// 2 func
+
+int myrand(const int& min, const int& max) {
+	return min > 0 ?
+		rand() % (max - min + 1) + min :
+		rand() % (abs(min) + max + 1) + min;
 }
 
 void full_mass(int* mass, int size, const int* dia) {
@@ -46,6 +48,39 @@ int max_(int* mass, int size) {
 	else return mass[0];
 }
 
+// 2 template
+
+template <typename T>
+T myrand_(const T& min, const T& max) {
+	return (T)(min > 0 ?
+		rand() % (max - min + 1) + min :
+		rand() % (abs(min) + max + 1) + min);
+}
+
+
+template <typename T>
+void full_mass_(T* mass, int size, const T* dia) {
+	mass[size - 1] = myrand(dia[0], dia[1]);
+	if (size > 1)
+		full_mass_<T>(mass, size - 1, dia);
+}
+
+template <typename T>
+void draw_mass_recurs_(const T* mass, const int& size, const int& c_space) {
+	if (size > 1)
+		draw_mass_recurs<T>(mass, size - 1, c_space);
+
+	cout << "|" << setw(c_space - 1) << mass[size - 1];
+}
+
+template <typename T>
+void draw_mass_(const T* mass, const int& size, const int& c_space) {
+	draw_line(size * c_space + 2);
+	draw_mass_recurs(mass, size, c_space);
+	cout << " |" << endl;
+	draw_line(size * c_space + 2);
+}
+
 template < typename T >
 T max__(T* mass, int size) {
 	if (--size > 0) {
@@ -66,9 +101,14 @@ void main() {
 
 	int* mass = new int[n];
 
+	cout << "1 // funcs" << endl;
 	full_mass(mass, n, dia);
 	draw_mass(mass, n, c_space);
 	cout << "max: " << max_(mass, n) << endl;
+
+	cout << "2 // tmpl funcs" << endl;
+	full_mass_(mass, n, dia);
+	draw_mass_(mass, n, c_space);
 	cout << "max tmpl: " << max__<int>(mass, n) << endl;
 
 	delete[] mass;
